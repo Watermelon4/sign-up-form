20f657187ba60a493e58399bc60698c740436b48
George Mao <zhongqilin@gmail.com>
2022-09-07 01:51:21 -0400

    add password comparison -add semicolons

--- scripts.js
+++ scripts.js
@@ -2,12 +2,6 @@ const userInputs = document.getElementsByClassName("user-input");
 const password = userInputs[4];
 const confirmPassword = userInputs[5];
 
-for (let i = 0; i < 6; i++) {
-  userInputs[i].addEventListener("input", checkValid);
-};
-// password.addEventListener("change", comparePasswords)
-// confirmPassword.addEventListener("change", comparePasswords)
-
 /**
  * Toggles validity icons based on client-side validity.
  */
@@ -21,7 +15,7 @@ function checkValid() {
 
   if (this.value == "") {
     hideIcons(this)
-  }
+  };
 };
 
 /**
@@ -33,7 +27,7 @@ function enableCheckMark(inputField) {
   const icons = inputField.nextElementSibling;
   icons.children[1].style.visibility = "hidden"; // set cross hidden
   icons.children[0].style.visibility = "visible"; // set check visible
-}
+};
 
 /**
  * Toggles the icon states to have the cross mark visible and the check mark 
@@ -44,7 +38,7 @@ function enableCrossMark(inputField) {
   const icons = inputField.nextElementSibling;
   icons.children[0].style.visibility = "hidden"; // set cross hidden
   icons.children[1].style.visibility = "visible"; // set check visible
-}
+};
 
 /**
  * Sets visibility of both icons to hidden.
@@ -54,15 +48,45 @@ function hideIcons(inputField) {
   const icons = inputField.nextElementSibling;
   icons.children[0].style.visibility = "hidden"; // set cross hidden
   icons.children[1].style.visibility = "hidden"; // set check hidden
-}
+};
 
-/**
- * both
- * blank then hide
- * 
- * pass
- * valid
- * 
- * confirm
- * valid and matching
- */
+function compareToPassword() {
+  if (confirmPassword.validity.valid == true) {
+    if (confirmPassword.value == "") {
+      hideIcons(confirmPassword);
+    }
+    else if(password.value == "") {
+      enableCrossMark(confirmPassword);
+    }
+    else if(password.value == confirmPassword.value) {
+      enableCheckMark(confirmPassword);
+    }
+    else {
+      enableCrossMark(confirmPassword);
+    };
+  }
+  else if (confirmPassword.value == "") {
+    hideIcons(confirmPassword)
+  }
+  else {
+    enableCrossMark(confirmPassword)
+  };
+};
+
+function compareToConfirm() {
+  if (password.value == confirmPassword.value) {
+    enableCheckMark(confirmPassword);
+  }
+  else if (confirmPassword.value == "") {
+    hideIcons(confirmPassword);
+  }
+  else {
+    enableCrossMark(confirmPassword);
+  };
+};
+
+for (let i = 0; i < 6; i++) {
+  userInputs[i].addEventListener("input", checkValid);
+};
+confirmPassword.addEventListener("input", compareToPassword);
+password.addEventListener("input", compareToConfirm);
